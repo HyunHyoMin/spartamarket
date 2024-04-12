497f591a37a7e6d01e7b80d09ce8b44fdf77cf99
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-04-12 19:09:24 +0900

    홈 추가

--- spartamarket/urls.py
+++ spartamarket/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from pages import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
 ]
