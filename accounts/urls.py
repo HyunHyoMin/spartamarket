b2bc4b0adcb067ff45f8f5a2a10c4c865c1c200b
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-04-12 18:50:48 +0900

    회원탈퇴, 프로필 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,8 @@ from accounts import views
 app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('delete/', views.delete, name='delete'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('<int:pk>/profile/', views.profile, name='profile'),
 ]
