764148c2caa10e82c68948e665124b4e9707af3e
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-04-14 23:47:29 +0900

    회원탈퇴 시 서버 내 프로필 이미지 삭제

--- products/models.py
+++ products/models.py
@@ -22,3 +22,11 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
+
+
+class MarkedProduct(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.title}"
