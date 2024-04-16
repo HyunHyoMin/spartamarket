7b8f1665562304341e369323a765a36c1011ad70
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-04-16 09:43:11 +0900

    follow 기능 구현

--- users/views.py
+++ users/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 from accounts.models import User
 from products.models import Product
 from django.views.decorators.http import require_POST, require_http_methods
 
-# Create your views here.
 
+@login_required
 @require_http_methods(["GET"])
 def profile(request, username):
     user = get_object_or_404(User, username=username)
     products = user.like_products.all()
     context = {
-        'user': user,
+        'member': user,
         'products':products
     }
     return render(request, "users/profile.html", context)
 
 
+@login_required
 @require_http_methods(["GET", "POST"])
 def update_profile(request, username):
     user = get_object_or_404(User, username=username)
@@ -28,3 +30,15 @@ def update_profile(request, username):
         return redirect("users:profile", user.username)
     context ={'user' : user}
     return render(request, "users/update_profile.html", context)
+
+
+@login_required
+@require_POST
+def follow(request, member_pk):
+    member = get_object_or_404(User, pk=member_pk)
+    if member != request.user:
+        if member.followers.filter(pk=request.user.pk).exists():
+            member.followers.remove(request.user)
+        else:
+            member.followers.add(request.user)
+    return redirect("users:profile", username=member.username)
